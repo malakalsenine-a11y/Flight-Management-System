8f05d26fb8558526f739b8531b045cb765c9e898
Malakaka <malakalsenine@gmail.com>
2026-06-24 10:01:25 +0400

    add Booking class and add property for this class

--- soluition/project01/Models/Booking.cs
+++ soluition/project01/Models/Booking.cs
@@ -8,5 +8,12 @@ namespace project01.Models
 {
     public class Booking
     {
+        public int bookingId { get; set; } //Generated
+        public int passengerId { get; set; } //User Input
+        public int flightId { get; set; } //User Input
+        public string seatNumber { get; set; } //User Input
+        public string bookingDate { get; set; } //User Input
+        public decimal totalPrice { get; set; } //Calculated
+        public string status { get; set; } // Default Value
     }
 }
