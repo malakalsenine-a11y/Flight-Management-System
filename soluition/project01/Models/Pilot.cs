947e917228c522e5a6688bb74b2949c6a5093788
Malakaka <malakalsenine@gmail.com>
2026-06-24 09:38:01 +0400

    add Pilot class and add property for this class

--- soluition/project01/Models/Pilot.cs
+++ soluition/project01/Models/Pilot.cs
@@ -8,5 +8,12 @@ namespace project01.Models
 {
     public class Pilot
     {
+        public int pilotId { get; set; } //Generated
+        public string pilotName { get; set; } // User Input
+        public string pilotPhone { get; set; } //User Input
+        public string licenseNumber { get; set; } // User Input
+        public int flightHours { get; set; } //Calcualted
+        public bool isAvailable { get; set; } //Default Value
+
     }
 }
