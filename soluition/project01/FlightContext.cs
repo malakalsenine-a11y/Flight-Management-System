e6aa6860d877bcba1cf679676f089e3096635134
Malakaka <malakalsenine@gmail.com>
2026-06-24 10:11:11 +0400

    Create a single FlightContext class with list fgor all class

--- soluition/project01/FlightContext.cs
+++ soluition/project01/FlightContext.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using project01.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,15 @@ namespace project01
 {
     public class FlightContext
     {
+        public List<Pilot> pilots { get; set; }
+        public List<Passenger> passengers { get; set; }
+        public List<Flight> flights { get; set; }
+        public List<Booking> bookings { get; set; }
+        public List<Aircraft> aircraft { get; set; }
+
+
+
+
+
     }
 }
--- soluition/project01/Models/Flight.cs
+++ soluition/project01/Models/Flight.cs
@@ -14,13 +14,11 @@ namespace project01.Models
         public int pilotId { get; set; } //User Input
         public string origin { get; set; } //User Input
         public string destination { get; set; } //User Input
-
         public string departureDate { get; set; } //User Input
-
         public string departureTime { get; set; } //User Input
-
         public decimal ticketPrice { get; set; } //User Input
         public int availableSeats { get; set; } //Default Value
         public string status { get; set; } // Default Value
+        public string flightDuration { get; set; } //Calculated
     }
 }
