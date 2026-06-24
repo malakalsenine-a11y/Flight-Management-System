b007fe6d796983ff3c6335bf8a45e26f92fb6ec4
Malakaka <malakalsenine@gmail.com>
2026-06-24 09:34:40 +0400

    add Passenger class and add property for this class

---
+++ soluition/project01/Models/Passenger.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace project01.Models
+{
+    public class Passenger
+    {
+        public int passengerId { get; set; } //Generated
+        public string passengerName { get; set; } // User Input
+        public string passengerEmail { get; set; } //User Input
+        public string passengerPhone { get; set; } // User Input
+        public string passportNumber { get; set; } //User Input
+        public string nationality { get; set; } // User Input
+
+    }
+}
+
+}
+}
