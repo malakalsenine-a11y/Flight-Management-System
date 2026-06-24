17ecb0dba8946bedfaeeb0a970017c4d8e829155
Malakaka <malakalsenine@gmail.com>
2026-06-24 09:49:40 +0400

    add Aircraft class and add property for this class

--- soluition/project01/Models/Aircraft.cs
+++ soluition/project01/Models/Aircraft.cs
@@ -8,5 +8,24 @@ namespace project01.Models
 {
     public class Aircraft
     {
+        public int flightId { get; set; } //Generated
+        public string flightCode { get; set; } // Default Value
+        public int aircraftId { get; set; } //User Input
+        public int pilotId { get; set; } //User Input
+        public string origin { get; set; } //User Input
+        public string destination { get; set; } //User Input
+
+        public string departureDate { get; set; } //User Input
+
+        public string departureTime { get; set; } //User Input
+
+        public decimal ticketPrice { get; set; } //User Input
+        public int availableSeats { get; set; } //Default Value
+        public string status { get; set; } // Default Value
+
+
+
     }
+
+}
 }
